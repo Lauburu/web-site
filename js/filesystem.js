046c825d24299c795b354426ee17c8633eee4659
Santiago Lorente Rey <cromatdragon@cromat-dragon.net>
2015-01-25 20:00:54 +0100

    Ahora tenemos menus anidados :D

--- js/filesystem.js
+++ js/filesystem.js
@@ -18,5 +18,24 @@ function DisplayTime(){ // Show current time dynamic
 	}
 
 $(document).ready(function () {
-
+	$(".dropdown li").on("click", "span", function(){
+		if($(this).hasClass("caret-right")){
+			$(this).removeClass("caret-right");
+			$(this).addClass("caret");
+		}else{
+			$(this).removeClass("caret");
+			$(this).addClass("caret-right");
+			}
+		});
+		
+		
+	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+	    // Avoid following the href location when clicking
+	    event.preventDefault(); 
+	    // Avoid having the menu to close when clicking
+	    event.stopPropagation(); 
+	    // Re-add .open to parent sub-menu item
+	    $(this).parent().addClass('open');
+	    $(this).parent().find("ul").parent().find("li.dropdown").addClass('open');
+		});
 	});
