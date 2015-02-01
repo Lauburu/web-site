037e33b909a2295fb334e736c1b1e450abf2a7fa
Santiago Lorente Rey <cromatdragon@cromat-dragon.net>
2015-02-01 15:53:59 +0100

    Añadidas transicciones entre el contenido del menú

--- js/filesystem.js
+++ js/filesystem.js
@@ -1,4 +1,4 @@
-function DisplayTime(){ // Show current time dynamic
+function DisplayTime(){ // Show current time dynamically
 	var CurrentDate=new Date();
 	var hours=CurrentDate.getHours();
 	var minutes=CurrentDate.getMinutes();
@@ -16,26 +16,58 @@ function DisplayTime(){ // Show current time dynamic
 	document.getElementById('current_time').innerHTML=currentTime;
 	setTimeout("DisplayTime()",1000);
 	}
+	
+function pc(type) { // Call to pc profiles
+	$.ajax({
+		type: "POST",
+		url: "php/lauburu_pc.php",
+		data: "pc="+type,
+		async: false,
+		success: function(data){ $("main").html(data)}
+		});
+	}
+	
+function soft(type,name) { // Call to software
+	$.ajax({
+		type: "POST",
+		url: "php/lauburu_software.php",
+		data: "soft="+type+"&name="+name,
+		async: false,
+		success: function(data){ $("main").append(data)}
+		});
+	}
 
 $(document).ready(function () {
-	$(".dropdown li").on("click", "span", function(){
-		if($(this).hasClass("caret-right")){
-			$(this).removeClass("caret-right");
-			$(this).addClass("caret");
-		}else{
-			$(this).removeClass("caret");
-			$(this).addClass("caret-right");
-			}
+	$("ul.dropdown-menu [data-toggle=dropdown]").on("click", function(event) {
+	    event.preventDefault(); 													// Avoid following the href location when clicking
+	    event.stopPropagation();										   			// Avoid having the menu to close when clicking
+	    $(this).parent().addClass("open");											// Re-add .open to parent sub-menu item
+	    $(this).parent().find("ul").parent().find("li.dropdown").addClass("open");
 		});
 		
-		
-	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
-	    // Avoid following the href location when clicking
-	    event.preventDefault(); 
-	    // Avoid having the menu to close when clicking
-	    event.stopPropagation(); 
-	    // Re-add .open to parent sub-menu item
-	    $(this).parent().addClass('open');
-	    $(this).parent().find("ul").parent().find("li.dropdown").addClass('open');
+	$("li > a").on("click", function(e){
+		e.preventDefault();
+		if ( $(this).data("type") === "PC") { 										// If PCprofile is clicked
+			pc($(this).data("name"));
+			} 			
+		else if ( $(this).data("type") ){ 											// If Software is clicked ...
+			$("#ShowPC").remove();
+			// Si hay un PC cargado, ¿como borrarlo para mostrar la info del software?
+			if( $(this).closest("ul").data("ftype") == 0 ){ 						// ... and another one is active and is his 1st time, enable
+				soft($(this).data("type"),$(this).data("name"));
+				$(this).closest("ul").data("ftype",1);
+				$(this).data("fname",1);
+			}else {
+				if( $(this).data("fname") == 1){ 									// ... and is his 2nd time, diseable
+					$("section[data-name='" + $(this).data("name") +"']").remove();
+					$(this).closest("ul").data("ftype",0);
+					$(this).data("fname",0);
+				}else{ 																// ... and was not chosen before, enable
+					$("section[data-type='" + $(this).data("type") +"']").remove();	
+					soft($(this).data("type"),$(this).data("name"));				
+					$(this).data("fname",1);
+					}
+				}
+			}
 		});
 	});
